5b20aca7183318ad7ae427a087f040c7de186bcb
Lucas Paiano <lucaspaiano@simplistic.com>
2021-10-26 20:52:58 -0300

    clase 3

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Hola, Coder! Soy Lucas Paiano!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Item de lista" />
+        <Button title="Add" />
+      </View>
+      
+      <View>
+        
+      </View>
     </View>
   );
 }
@@ -17,5 +25,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft: 20,
+    paddingRight: 20
   },
+  inputContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  textInput:{
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    flex: 1,
+    marginRight: 20
+  }
 });
