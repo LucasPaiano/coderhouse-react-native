32b1dc636bc21876c81d3672fc987b4da47dd571
Lucas Paiano <lucaspaiano@simplistic.com>
2021-11-02 19:06:48 -0300

    clase 4

--- App.tsx
+++ App.tsx
@@ -1,42 +1,152 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  Button,
+  Text,
+  View,
+  FlatList,
+  Modal,
+} from 'react-native';
 
 export default function App() {
-  return (
+  const [inputText, setInputText] = useState('');
+  const [itemList, setItemList] = useState([]);
 
-    <View style={styles.container}>
-      
+  const [itemSelected, setItemSelected] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleChangeText = (text) => setInputText(text);
+  const handleAddItem = () => {
+
+    // no se usa push porque push solamente modifica, lo que hay que hacer es crear un valor nuevo (usando una copia del anterior + el valor nuevo)
+    setItemList([...itemList,{id: Math.random().toString(), value: inputText,},]);
+    // se resetea textValue
+    setInputText('');
+  }
+
+  const handleConfirmDelete = () => {
+    const id = itemSelected.id;
+    setItemList(itemList.filter(item => item.id !== id));
+    setModalVisible(false);
+    setItemSelected({});
+  }
+
+  const handleModal = id => {
+    setItemSelected(itemList.find(item => item.id === id));
+    setModalVisible(true);
+  }
+
+  return (
+    <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Item de lista" />
-        <Button title="Add" />
+        <TextInput
+          placeholder="Agregar item"
+          style={styles.input}
+          onChangeText={handleChangeText}
+          value={inputText}
+        />
+        <Button
+          title="ADD"
+          color="#3D9970"
+          onPress={handleAddItem}
+        />
       </View>
-      
-      <View>
-        
-      </View>
+      <FlatList
+        data={itemList}
+        renderItem={data => {
+          return (
+            <View style={[styles.item, styles.shadow]}>
+              <Text>{data.item.value}</Text>
+              <Button
+                title="X"
+                color="#AAAAAA"
+                onPress={() => handleModal(data.item.id)}
+              />
+            </View>
+          );
+        }}
+        keyExtractor={item => item.id}
+      />
+      <Modal animationType="slide" visible={modalVisible} transparent>
+        <View style={styles.modalContainer}>
+          <View style={[styles.modalContent, styles.shadow]}>
+            <Text style={styles.modalMessage}>¿Está seguro que desea borrar?</Text>
+            <Text style={styles.modalTitle}>{itemSelected.value}</Text>
+            <View>
+              <Button
+                onPress={handleConfirmDelete}
+                title="CONFIRMAR"
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
+    padding: 30,
+    backgroundColor: '#F0F0F0',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingLeft: 20,
-    paddingRight: 20
   },
-  inputContainer:{
+  inputContainer: {
     flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-between',
-    alignItems: 'center'
   },
-  textInput:{
+  input: {
     borderBottomColor: 'black',
     borderBottomWidth: 1,
+    width: 200,
+  },
+  items: {
+    marginTop: 20,
+  },
+  item: {
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: 'white',
+  },
+  modalContainer: {
     flex: 1,
-    marginRight: 20
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalContent: {
+    padding: 30,
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalMessage: {
+    fontSize: 18,
+  },
+  modalTitle: {
+    fontSize: 30,
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   }
 });
